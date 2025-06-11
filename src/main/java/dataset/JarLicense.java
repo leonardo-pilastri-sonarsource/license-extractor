c806d57ad41ad1628ca48d73437d05535bc8283d
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2025-06-11 10:51:15 +0200

    Also search license in pom file on mvn repo

--- src/main/java/dataset/JarLicense.java
+++ src/main/java/dataset/JarLicense.java
@@ -1,12 +1,23 @@
 package dataset;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.zip.ZipInputStream;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class JarLicense {
+
+  private static final HttpClient CLIENT = HttpClient.newHttpClient();
 
   public String licenseFileContent;
 
@@ -38,6 +49,54 @@ public class JarLicense {
     } catch (IOException e) {
       System.err.println("Error reading " + artifact.localPath());
     }
+    if (licenseFileContent == null) {
+      String pomUrl = buildPomUrl(artifact.groupId(), artifact.artifactId(), artifact.version());
+      try {
+        Document pom = downloadPom(pomUrl);
+        licenseFileContent = extractLicenseName(pom);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+
+    }
+  }
+
+  public static String extractLicenseName(Document doc) throws Exception {
+    XPath xp = XPathFactory.newInstance().newXPath();
+    // Match: /project/licenses/license/name but using local-name() to ignore xmlns
+    String expr = "/*[local-name()='project']"
+      + "/*[local-name()='licenses']"
+      + "/*[local-name()='license']"
+      + "/*[local-name()='name']/text()";
+    NodeList nodes = (NodeList) xp.evaluate(expr, doc, XPathConstants.NODESET);
+
+    if (nodes.getLength() > 0) {
+      return nodes.item(0).getNodeValue().trim();
+    }
+    return null;
+  }
+
+  private static Document downloadPom(String pomUrl) throws Exception {
+    HttpRequest req = HttpRequest.newBuilder()
+      .uri(URI.create(pomUrl))
+      .GET()
+      .build();
+    HttpResponse<InputStream> resp = CLIENT.send(req, HttpResponse.BodyHandlers.ofInputStream());
+    if (resp.statusCode() != 200) {
+      throw new RuntimeException("Failed to fetch POM: HTTP " + resp.statusCode());
+    }
+    // parse
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    dbf.setNamespaceAware(true);
+    return dbf.newDocumentBuilder().parse(resp.body());
+  }
+
+  private static String buildPomUrl(String groupId, String artifactId, String version) {
+    String path = groupId.replace('.', '/');
+    return String.format(
+      "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s.pom",
+      path, artifactId, version, artifactId, version
+    );
   }
 
 }
